c8e3740d0f1bab1a4fa84129ee21234409c4e188
Jethro Williams <jethrowilliams@outlook.com>
2021-10-27 11:05:32 +0100

    Fetch the meals from the database

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,49 +1,64 @@
+import { useEffect, useState, useCallback } from 'react';
+
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem
-      key={meal.id}
-      id={meal.id}
-      name={meal.name}
-      description={meal.description}
-      price={meal.price}
-    />
-  ));
+  const [meals, setMeals] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const fetchMealsHandler = useCallback(async () => {
+    try {
+      const response = await fetch('https://react-http-94026-default-rtdb.europe-west1.firebasedatabase.app/meals.json')
+
+      if (!response.ok) {
+        throw new Error('Hope you like being hungry');
+      }
+
+      const data = await response.json();
+
+      const returnedMeals = [];
+
+      for (const key in data) {
+        returnedMeals.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      };
+
+      setMeals(returnedMeals);
+    } catch (error) {
+      setErrorMessage(error.message)
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchMealsHandler();
+  }, [fetchMealsHandler]);
+
+  let content = <p>{errorMessage}</p>
+  if (meals.length > 0) {
+    content =
+    <ul>
+      {meals.map((meal) => (
+        <MealItem
+          key={meal.id}
+          id={meal.id}
+          name={meal.name}
+          description={meal.description}
+          price={meal.price}
+        />
+      ))};
+    </ul>
+  }
 
   return (
     <section className={classes.meals}>
       <Card>
-        <ul>{mealsList}</ul>
+        {content}
       </Card>
     </section>
   );
